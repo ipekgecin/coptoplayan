137c91ea39f40244cce41a4bd5d716bbaffcc98b
Kaan Arı <kaanari@hacettepe.edu.tr>
2018-01-04 21:57:49 +0300

    Updated

--- renksensoru/renksensoru.ino
+++ renksensoru/renksensoru.ino
@@ -24,8 +24,9 @@ void loop() {
 }
 void findcolor(){
 // Setting red filtered photodiodes to be read
-  int r=0,g=0,b=0;
-  for(int i=0;i<3;i++){
+  char color;
+  int red=0,green=0,blue=0;
+  for(int i=0;i<10;i++){
     digitalWrite(S2,LOW);
     digitalWrite(S3,LOW);
     redFrequency = pulseIn(sensorOut, LOW);
@@ -41,20 +42,29 @@ void findcolor(){
     blueFrequency = pulseIn(sensorOut, LOW);
     blueFrequency = map(blueFrequency,27,350,255,0);
     delay(100);
-    r=redFrequency+r;
-    b=blueFrequency+b;
-    g=greenFrequency+g;
+    red=redFrequency+red;
+    blue=blueFrequency+blue;
+    green=greenFrequency+green;
   }
-  r=r/3;
-  g=g/3;
-  b=b/3;
-  Serial.print("R=");
-  Serial.print(r);
-  Serial.print(" G=");
-  Serial.print(g);
-  Serial.print(" B=");
-  Serial.print(b);
-  Serial.println(" ");
-  delay(100);
+  red=red/10;
+  green=green/10;
+  blue=blue/10;
+  //Serial.print("R=");
+  //Serial.print(r);
+  //Serial.print(" G=");
+  //Serial.print(g);
+  //Serial.print(" B=");
+  //Serial.print(b);
+  //Serial.println(" ");
+  //delay(100);
+  if (red > 8  && red < 18   &&  green >  9 && green < 19    &&  blue > 8  && blue < 16)   color = "WHITE";
+  else if (red > 80 && red < 125  &&  green > 90 && green < 125   &&  blue > 80 && blue < 125)  color = "BLACK";
+  else if (red > 12 && red < 30   &&  green > 40 && green < 70    &&  blue > 33 && blue < 70)   color = "RED";
+  else if (red > 50 && red < 95   &&  green > 35 && green < 70    &&  blue > 45 && blue < 85)   color = "GREEN";
+  else if (red > 10 && red < 20   &&  green > 10 && green < 25    &&  blue > 20 && blue < 38)   color = "YELLOW";
+  else if (red > 65 && red < 125  &&  green > 65 && green < 115   &&  blue > 32 && blue < 65)   color = "BLUE";
+  else  color = "NO_COLOR";
+  Serial.print(color);
+  
 }
 

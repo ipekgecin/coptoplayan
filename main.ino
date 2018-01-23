8513d139036cba5b30b90f994c4a45760cebdf97
Kaan Arı <33496550+kaanaritr@users.noreply.github.com>
2018-01-23 15:00:57 +0300

    Main File
    Pinler tanımlandı.

---
+++ main.ino
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------------------
+//Hazne Renk Sensörü
+#define hs_S0 22
+#define hs_S1 24
+#define hs_S2 26
+#define hs_S3 28
+#define hs_SOut 30
+//Çöp Kutu Renk Sensörü
+#define ks_S0 23
+#define ks_S1 25
+#define ks_S2 27
+#define ks_S3 29
+#define ks_SOut 31
+//Hazne Mesafe Sensörü
+#define hs_echo 32
+#define hs_trig 33
+//Sağ Mesafe Sensörü
+#define sags_echo 34//
+#define sags_trig 35
+//Sol Mesafe Sensörü
+#define sols_echo 36
+#define sols_trig 37
+//Ön Mesafe Sensörü
+#define ons_echo 38
+#define ons_trig 39
+//Motorlar
+#define sol_motor1 40
+#define sol_motor2 41
+#define sag_motor1 42
+#define sag_motor2 43
+//Servolar
+#define kapak_serv 44
+#define kol_serv 45
+//-----------------------------------------------------------------------------------------------
